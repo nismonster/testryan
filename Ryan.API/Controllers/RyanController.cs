97189a8c77ee53ff75bdc16b1cf388defe1e64c0
Adam <adam.hammad@gmail.com>
2024-10-18 14:01:30 -0400

    init

---
+++ Ryan.API/Controllers/RyanController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ryan.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RyanController : ControllerBase
+    {
+        public RyanController()
+        {
+        }
+
+        [HttpGet("test")]
+        public async Task<ActionResult> TestPing()
+        {
+            Random random = new Random();
+            var rand = random.Next(random.Next(100));
+            return Ok($"Test response {rand}");
+        }
+    }
+}
---
+++ Ryan.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("About to call API...");
+            Console.ReadLine();
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5002");
+            var response = await client.GetAsync("/api/Ryan/test");
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Got success from API");
+                var realResult = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"{realResult}");
+            }
+            else
+            {
+                Console.WriteLine("Got error from API");
+            }
+            Console.ReadLine();
+        }
+    }
+}
